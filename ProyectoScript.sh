c590b362c35e96653ae71a0aa706b9f4f95ed1cc
julangar <jagarciaguer@msn.com>
2021-02-23 12:20:32 -0500

    Terminado

---
+++ ProyectoScript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "\033[33mSe instalará python3, pip3 y VS Code\033[00m"
+echo "\033[33mPrimero se hara un update y un upgrade para actualizar los repositorios\033[00m"
+echo "\033[33mUsando como super usuario los comandos: \033[00m"
+echo "\033[31msudo apt update y sudo apt upgrade\n\033[00m"
+sudo apt update
+sudo apt upgrade -y
+echo "\033[33mAhora se instalará python3 con el comando: \033[00m"
+echo "\033[31msudo apt install python3\n\033[00m"
+sudo apt install python3
+echo "\033[33mSe instala el gestor de paquetes de python pip con el comando: \033[00m"
+echo "\033[31msudo apt install python3-pip\n\033[00m"
+sudo apt install python3-pip
+echo "\033[33mSe instala el entorno de desarrollo de VS Code mediante el comando: \033[00m"
+echo "\033[31msnap install --classic code\n\033[00m"
+sudo snap install --classic code
+echo "\033[33mPor ultimo se instala la extensión de python para VS Code con el comando: \033[00m"
+echo "\033[31mcode -- install-extension ms-python.python\n\033[00m"
+code --install-extension ms-python.python
+echo "\033[33mAbrimos VS Code con el comando: \033[00m"
+echo "\033[31code\n\033[00m"
+code
+exit
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Script Python-VsCode
- Script que automatiza la instalación de python y VS Code en distribuciones Debian
+# Script Instalación Python-VsCode
+## Script que automatiza la instalación de python y VS Code en distribuciones Debian
+## Se debe tener en cuenta que para la correcta ejecucion del script se debe tener el gestor de paquetes snap
+
+# Se utilizan los siguientes comandos:
+
+- sudo apt upgrade
+- sudo apt update -y 
+- sudo apt install python3
+- sudo apt install python3-pip
+- sudo snap install --classic code
+- code --install-extension ms-python.python
+
+
+### Julian Andres Garcia Guerrero
+### Cod: 20171020011
+### Universidad Distrital Francisco Jose de Caldas
+### Ingenieria de Sistemas
